4787d769ca14b3a04109b5f003dfb6e0508fce07
you@example.com <you@example.com>
2023-10-25 10:34:06 +0700

    add antd custom, mark version, pre commit build

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,37 @@ import i18next from './assets/locales';
 import { Provider } from 'react-redux';
 import { reduxStore } from './assets/redux/store';
 import CommonComponent from './routers/common';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <I18nextProvider i18n={i18next}>
-      <Provider store={reduxStore}>
-        <CommonComponent>
-          <Routers />
-        </CommonComponent>
-      </Provider>
-    </I18nextProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#7474f6cc',
+        },
+        components: {
+          Layout: {},
+          Menu: {
+            // if you use "dark" theme on menu
+          },
+          Button: {
+            colorLink: '#7474f6cc',
+            colorLinkHover: '#7474f6',
+          },
+        },
+      }}
+    >
+      <I18nextProvider i18n={i18next}>
+        <Provider store={reduxStore}>
+          <CommonComponent>
+            <Routers />
+          </CommonComponent>
+        </Provider>
+      </I18nextProvider>
+    </ConfigProvider>
   </StrictMode>
 );
